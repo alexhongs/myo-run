356379fd919b5607a6be90b8d9532f173bb01b31
Alex Hong <seongkwh@andrew.cmu.edu>
2020-09-19 10:37:41 -0400

    Simple Ball controlled with Up, Down, Left, Right implemented - Updated .gitignore - Updated README on instructions to open Unity project

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 Myoelectric Runner Game
 
+- Add .gitignore separately in individual projects
+
 ### Digital Signal Processing and Machine Learning
 
 ### Unity Game Project
+Opening Unity Game Project
+1. Download Unity Hub from (https://unity3d.com/get-unity/download)
+2. Open Unity Hub
+3. Click Projects > Add > go into myo-run directory > click Unity Project and click Open
+4. Open Project
+5. Under Projects Tab, go to Assets > Scenes > and click Scenes to load project
+6. Click Play button to start game
---
+++ simpleBallGame/Assets/Scripts/MainController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mag = 10f * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Translate(-mag, 0f, 0f);
+            print("Pressed Left");
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Translate(mag, 0f, 0f);
+            print("Pressed Right");
+        }
+        else if(Input.GetKey(KeyCode.UpArrow)) {
+            transform.Translate(0f, mag, 0f);
+            print("Pressed Up");
+        }
+        else if(Input.GetKey(KeyCode.DownArrow)) {
+            transform.Translate(0, -mag, 0f);
+            print("Pressed Down");
+        }
+    }
+}
