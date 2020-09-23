6e74efeef3a5f8a40733105dc39969f3e93d97a1
alexhongs <seongkwh@andrew.cmu.edu>
2020-09-23 17:17:48 -0400

    Realistic mock data server client working (#3)
    Server
    - testStream() - sends mock inputs 0000 1111 0000 2222 ....
    
    Client
    - Rising and Falling edge detection
    
    Usage
    
    ```
    // On Shell 1
    python3
    exec(open("server.py").read())
    
    // On Shell 2
    python client.py
    
    // On Shell 1
    testStream()
    ```

--- connection/server.py
+++ connection/server.py
@@ -1,33 +1,54 @@
 import socket
 
-UDP_IP_ADDRESS = "127.0.0.1"
-UDP_PORT = 18500
-BUFFER_SIZE = 1024
+###### Interface ######
+def connect():
+  global address
+  print("UDP Server up and listening" )
+  print("Waiting for request")
+  data, address = UDPServerSocket.recvfrom(BUFFER_SIZE)
 
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+  # data, address = UDPServerSocket.recvfrom(BUFFER_SIZE)
+  print("Data from Client: {}".format(data))
+  print("Client IP Address: {}".format(address))
 
-UDPServerSocket.bind((UDP_IP_ADDRESS, UDP_PORT))
-
-print("UDP Server up and listening")
-
-print "Waiting for request"
-data, address = UDPServerSocket.recvfrom(BUFFER_SIZE)
-print "Data from Client: {}".format(data)
-print "Client IP Address: {}".format(address)
-
-# Stream in data to client as soon as the connection is open
-count = 0
-while (True):
-  msgFromServer       = "Hello UDP Client | packet number: " + str(count)
-  bytesToSend         = str.encode(msgFromServer)
-
-  UDPServerSocket.sendto(bytesToSend, address)
-  count += 1
-
-
-# TODO: Need to stream in real-time data. Perhaps a singler liner function sendData() that just sends to client might suffice
-# TODO: Need to mock real data and send that. We may also be able to get this in interpreter mode where sendData() is called to
-# test control game could work
 def sendData(data):
   bytesToSend = str.encode(data)
   UDPServerSocket.sendto(bytesToSend, address)
+
+###### Initialization ######
+UDP_IP_ADDRESS = "127.0.0.1"
+UDP_PORT = 18500
+BUFFER_SIZE = 1024
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPServerSocket.bind((UDP_IP_ADDRESS, UDP_PORT))
+address = ""
+connect()
+
+##################
+###### Test ######
+##################
+# test sending numbers 1 - 10000
+def testSend():
+  for i in range(10000):
+    sendData(str(i))
+
+# test sending all inputs incrementally, FLEX, RELAX, EXTEND, RELAX ( loop )
+def testStream():
+  RELAX = 0
+  FLEXION = 1
+  EXTENSION = 2
+
+  i = 0
+  while(True):
+
+    if(i < 1000):
+      sendData(str(FLEXION))
+    elif (5000 <= i < 10000):
+      sendData(str(EXTENSION))
+    else:
+      sendData(str(RELAX))
+    
+    if(20000 < i):
+      i = 0
+    i+=1
+
