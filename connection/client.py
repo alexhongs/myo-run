0c2f35cd6abcfd5d7270fdb2bdd7200a10934920
Alex Hong <seongkwh@andrew.cmu.edu>
2020-09-29 23:26:02 -0400

    UDP Client Server model changed Before: - UDP Server: Muscle Classifier - UDP Client: Unity
    After:
    - UDP Server: Unity
    - UDP Client: Muscle Classifier
    
    1. New Client simply broadcast/streams to a specified address and port
    2. New Server - who is binded to the specified address and port - receives data from Client
    3. New Server doesn't send anything back

--- connection/client.py
+++ connection/client.py
@@ -12,12 +12,12 @@ bytesToSend         = str.encode(msgFromClient)
 
 # Create a UDP socket at client side
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPClientSocket.sendto(bytesToSend, UDP_ADDRESS_PORT)
+UDPClientSocket.bind(UDP_ADDRESS_PORT)
 
 # Loop listening for responses from udp server
 while (True):  
   data, address = UDPClientSocket.recvfrom(BUFFER_SIZE)
-  msg = "Server Response: {}".format(data)
+  msg = "Server Response: {}".format(data) + " | " + str(address)
   if (current == RELAX and current != data): # rising edge
     print("Rising Edge" + msg)
     current = data
@@ -26,3 +26,31 @@ while (True):
     print("Falling Edge " + msg)
     current = data
   
+
+############################################################################################################################################
+## 9/29/2020 - Note on not binding on server.py / Note on binding on client.py
+#
+# The classical UDP server client model suggests having the server bind to a "well-known" port address, 
+# and sends data to client only after client first makes requests.
+# 
+# Helpful Image of Classical UDP Diagram
+# https://stackoverflow.com/questions/23068905/is-bind-necessary-if-i-want-to-receive-data-from-a-server-using-udp
+# 
+# Classical UDP Server Client model 
+# <Server Side>
+# 1. data, address = UDPServerSocket.recvfrom(BUFFER_SIZE)
+
+# and then send packet to 
+# 2. UDPServerSocket.sendto(bytesToSend, address)
+
+# This means that you can't send any data until you get the address of the UDP Client. 
+# This is inefficient because every time a server or client is disconnected, the entire 
+# connection process needs to be done again.
+
+
+# This probably means that server-client model be swapped around, so Signal Processor/ ML side is now the client and Unity is the server.
+# New UDP Client: DSP/ML Classifier
+# New UDP Server: Unity Receiver
+# 1. Client simply broadcast/streams to a specified address and port
+# 2. Server - who is binded to the specified address and port - receives data from Client
+# 3. Server doesn't send anything back
--- connection/server.py
+++ connection/server.py
@@ -1,16 +1,6 @@
 import socket
 
 ###### Interface ######
-def connect():
-  global address
-  print("UDP Server up and listening" )
-  print("Waiting for request")
-  data, address = UDPServerSocket.recvfrom(BUFFER_SIZE)
-
-  # data, address = UDPServerSocket.recvfrom(BUFFER_SIZE)
-  print("Data from Client: {}".format(data))
-  print("Client IP Address: {}".format(address))
-
 def sendData(data):
   bytesToSend = str.encode(data)
   UDPServerSocket.sendto(bytesToSend, address)
@@ -18,19 +8,10 @@ def sendData(data):
 ###### Initialization ######
 UDP_IP_ADDRESS = "127.0.0.1"
 UDP_PORT = 18500
-BUFFER_SIZE = 1024
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPServerSocket.bind((UDP_IP_ADDRESS, UDP_PORT))
-address = ""
-connect()
+address = ('127.0.0.1', 18500)
 
-# while (True):
-#   sendData(str(1));
-
-
-##################
 ###### Test ######
-##################
 # test sending numbers 1 - 10000
 def testSend():
   for i in range(10000):
@@ -56,3 +37,32 @@ def testStream():
       i = 0
     i+=1
 
+
+
+############################################################################################################################################
+## 9/29/2020 - Note on not binding on server.py / Note on binding on client.py
+#
+# The classical UDP server client model suggests having the server bind to a "well-known" port address, 
+# and sends data to client only after client first makes requests.
+# 
+# Helpful Image of Classical UDP Diagram
+# https://stackoverflow.com/questions/23068905/is-bind-necessary-if-i-want-to-receive-data-from-a-server-using-udp
+# 
+# Classical UDP Server Client model 
+# <Server Side>
+# 1. data, address = UDPServerSocket.recvfrom(BUFFER_SIZE)
+
+# and then send packet to 
+# 2. UDPServerSocket.sendto(bytesToSend, address)
+
+# This means that you can't send any data until you get the address of the UDP Client. 
+# This is inefficient because every time a server or client is disconnected, the entire 
+# connection process needs to be done again.
+
+
+# This probably means that server-client model be swapped around, so Signal Processor/ ML side is now the client and Unity is the server.
+# New UDP Client: DSP/ML Classifier
+# New UDP Server: Unity Receiver
+# 1. Client simply broadcast/streams to a specified address and port
+# 2. Server - who is binded to the specified address and port - receives data from Client
+# 3. Server doesn't send anything back
