1b04f8eb0e146714e3c7e3a250f0c317c53f42eb
Alex Hong <seongkwh@andrew.cmu.edu>
2020-10-27 12:52:41 -0400

    myo-run game initial commit

---
+++ myo-run/Assets/Scripts/RoadSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+// Road generator looping 7 Road Segments
+public class RoadSpawner : MonoBehaviour
+{
+    public List<GameObject> roads;
+    private float offset = 25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(roads != null && roads.Count > 0)
+        {
+            roads = roads.OrderBy(road => road.transform.position.z).ToList();
+        }   
+    }
+
+    public void PopAndPushRoad()
+    {
+        GameObject road = roads[0];
+        roads.Remove(road);
+        float newZ = roads[roads.Count - 1].transform.position.z + offset;
+        road.transform.position = new Vector3(0, 0, newZ);
+        roads.Add(road);
+    }
+}
---
+++ myo-run/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    RoadSpawner roadSpawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        roadSpawner = GetComponent<RoadSpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnTriggerEntered()
+    {
+        roadSpawner.PopAndPushRoad();
+    }
+}
